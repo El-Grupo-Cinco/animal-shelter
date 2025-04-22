d93fe0bf524a188f27baa2408ae1bbae4c170513
eckofox1981 <franklineck@pm.me>
2025-04-22 14:57:05 +0200

    added some functionality and display to admin mode of user page

--- frontend/javascript/user.js
+++ frontend/javascript/user.js
@@ -1,12 +1,14 @@
 import {User} from "./userClass.js";
 
+getUser();
 
 /**
  * fetches the user and shows it on the page
  */
 function getUser() {
-let testUser = new User("25135213516516dcxazs", "Testing", "Cesar", "Milan", "2025-08-08", "1000 Hollywood Blvd, 90120 Beverly Hills", "dogwhisperer@discovery.com", "+ 62 65 19884 664", null, true, "user")
-
+let testUser = new User("1af40162-50ef-43e0-847f-ccf54700587a", "Testing", "Cesar", "Milan", "2025-08-08", 
+        "1000 Hollywood Blvd, 90120 Beverly Hills", "dogwhisperer@discovery.com", 
+        "+1 62 65 19884 664", [], true, "admin");
 testUser.showUser();
 
 }
--- frontend/javascript/userClass.js
+++ frontend/javascript/userClass.js
@@ -1,5 +1,6 @@
 export class User {
-        constructor(userID, username, firstname, lastname, dateOfBirth, address, email, phoneNumber, ownedAnimals, canAdopt, role) {
+        constructor(userID, username, firstname, lastname, dateOfBirth, address, 
+                email, phoneNumber, ownedAnimals, canAdopt, role) {
                 this.userID = userID;
                 this.username = username;
                 this.firstname = firstname;
@@ -40,5 +41,81 @@ export class User {
                         canAdoptH4.textContent = "No";
                 }
 
+                if (this.role === "admin") {
+                        this.showAdmin();
+                }
         }
+
+        showAdmin() {
+                //preparing for searchQuery
+                let searchQuery;
+                //Making element
+                const userInfoSection = document.getElementsByClassName("user-info");
+                const adminSection = document.createElement("section");
+                const searchLabel = document.createElement("label");
+                const searchInput = document.createElement("input");
+                const buttons = document.createElement("div");
+                const searchUserBtn = document.createElement("button");
+                const searchAnimalBtn = document.createElement("button");
+                const approveAdoptionBtn = document.createElement("button");
+                const deleteAdoptionBtn = document.createElement("button");
+                const bookingsBtn = document.createElement("button");
+
+                //setting atributes
+                searchLabel.setAttribute("for", "searchQuery");
+                searchInput.setAttribute("id", "searchQuery");
+                searchInput.setAttribute("placeholder", "Enter search term here" );
+
+                //setting text content
+                searchLabel.textContent = "Search Query";
+                searchUserBtn.textContent = "Search User";
+                searchAnimalBtn.textContent = "Search Animal";
+                approveAdoptionBtn.textContent = "Approve Adoption";
+                deleteAdoptionBtn.textContent = "Delete Adoption";
+                bookingsBtn.textContent = "Handle Bookings";
+
+                //setting style / class
+                adminSection.className = "admin-section";
+
+                //adding event-listener
+                searchInput.addEventListener('input', function() {searchQuery=searchInput.value; console.log(searchQuery);
+                })
+                searchUserBtn.addEventListener('click', function() {searchUser(searchQuery)});
+                searchAnimalBtn.addEventListener('click', function() {searchAnimal(searchQuery)});
+                approveAdoptionBtn.addEventListener('click', function() {goToApproveAdoption()});
+                deleteAdoptionBtn.addEventListener('click',function() {goToDeleteAdoption()});
+                bookingsBtn.addEventListener('click', function() {goToBooking()});
+
+                //appending to document
+                userInfoSection[0].append(adminSection);
+                adminSection.appendChild(searchLabel);
+                adminSection.appendChild(searchInput);
+                adminSection.appendChild(buttons);
+                buttons.appendChild(searchUserBtn);
+                buttons.appendChild(searchAnimalBtn);
+                buttons.appendChild(approveAdoptionBtn);
+                buttons.appendChild(deleteAdoptionBtn);
+                buttons.appendChild(bookingsBtn);
+                
+        }
+}
+
+function searchUser(searchQuery) {
+        console.log(searchQuery);
+}
+
+function searchAnimal(searchQuery) {
+        console.log(searchQuery);
+}
+
+function goToApproveAdoption() {
+        console.log("Approve Adoption button");
+}
+
+function goToDeleteAdoption() {
+        console.log("delete Adoption button");
+}
+
+function goToBooking() {
+        console.log("handle booking Adoption button");
 }
