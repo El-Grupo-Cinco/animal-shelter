1f00be68ff0cce53410bff8198f614f9cc8b1f3d
itsabunny <itsabunny@pm.me>
2025-04-25 00:39:17 +0200

    feat: add showAnimal method to Animal class for card generation

--- frontend/javascript/animalClass.js
+++ frontend/javascript/animalClass.js
@@ -9,5 +9,17 @@ export class Animal {
         this.description = description;
     }
 
-    
+    showAnimal(){
+        const cardTemplate = document.getElementById("card-template");
+        const newCard = cardTemplate.cloneNode(true);
+        newCard.classList.remove("hidden");
+        newCard.querySelector(".card-picture").src=this.pictureURL;
+        newCard.querySelector(".species-text").textContent=this.species;
+        newCard.querySelector(".date-of-birth-text").textContent=this.dateOfBirth;
+        newCard.querySelector(".registered-text").textContent=this.registrationDate;
+        newCard.querySelector(".card-name").textContent=this.name.toUpperCase();
+        newCard.querySelector(".card-description").textContent=this.description;
+        newCard.removeAttribute("id");
+        return newCard;
+    }
 }
