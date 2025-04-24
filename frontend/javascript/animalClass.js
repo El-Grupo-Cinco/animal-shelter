8e93a8f5c8c3a37956764217f4af8c70a8612eb5
itsabunny <itsabunny@pm.me>
2025-04-24 21:32:54 +0200

    feat: create Animal class for managing animal data

---
+++ frontend/javascript/animalClass.js
@@ -0,0 +1,13 @@
+export class Animal {
+    constructor(animalID, name, pictureURL, species, dateOfBirth, registrationDate, description){
+        this.animalID = animalID;
+        this.name = name;
+        this.pictureURL = pictureURL;
+        this.species = species;
+        this.dateOfBirth = dateOfBirth;
+        this.registrationDate = registrationDate;
+        this.description = description;
+    }
+
+    
+}
