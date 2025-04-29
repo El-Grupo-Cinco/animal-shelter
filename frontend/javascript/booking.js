d7c88ab10f1432a2f2ff1e390bf0eddf120d04b4
eckofox1981 <franklineck@pm.me>
2025-04-29 10:30:00 +0200

    added booking section and implemented BookingRequest and Booking, css accordingly

---
+++ frontend/javascript/booking.js
@@ -0,0 +1,11 @@
+import { BookingRequest, Booking } from "./bookingClass.js";
+
+const animalCards = document.getElementById("booking-cards");
+
+const bookings = [new Booking("DarthVader", "R2D2", "2025-05-08T08:00UTC", ["Bring bolts and charger", "Brings old picture from Tatoine"]),
+        new Booking("Longstocking", "Lilla Gubben", "2025-05-08T08:00UTC", ["Brings a monkey (why?)", "Is very strong, can carry the horse back if all goes well"])
+];
+
+for (const booking of bookings) {
+        animalCards.append(booking.publish());
+}
---
+++ frontend/javascript/bookingClass.js
@@ -0,0 +1,53 @@
+export class BookingRequest {
+        constructor(userId, animalId, appointmentTime, comments) {
+                this.userId = userId;
+                this.animalId = animalId;
+                this.appointmentTime = appointmentTime;
+                this.comments = comments;
+        }
+
+        save() {
+                console.log(`save booking for ${this.userId}`);
+        }
+
+        
+}
+
+export class Booking {
+        constructor (humanName, animalName, appointmentTime, comments) {
+                this.humanName = humanName;
+                this.animalName = animalName;
+                this.appointmentTime = appointmentTime,
+                this.comments = comments;
+        }
+
+        publish() {
+                const card = document.createElement("article");
+                const dateDiv = document.createElement("div");
+                const animalName = document.createElement("h2");
+                const userName = document.createElement("h2");
+                const commentList = document.createElement("ul");
+
+                card.className = "booking-card";
+
+                
+
+                dateDiv.textContent = this.appointmentTime;
+
+                userName.textContent = this.humanName;
+                
+                animalName.textContent = this.animalName;
+
+                commentList.textContent = "Comments";
+
+                for(const comment of this.comments) {
+                        const listItem = document.createElement("li");
+                        listItem.textContent = comment;
+                        commentList.append(listItem);
+                }
+
+                card.append(dateDiv, animalName, userName, commentList);
+
+                return card;
+        }
+}
